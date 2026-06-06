73e628241ce78c22fcc58d1edc04de5b36818332
gauravPanditr <gauravpandit7856@gmail.com>
2026-06-06 15:52:38 +0530

    we created image analysis service

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 
-
+from src.analyzer import analyze_image
 app = FastAPI(title="OpenCV Services")
 
 
 @app.get("/")
 def home():
     return {"status": "OpenCV Service Running"}
+
+
+@app.post("/analyze")
+async def analyze(file: UploadFile = File(...)):
+    result = await analyze_image(file)
+    return result
